1c42034137d53b9df04171df60aee8f35c21cc3d
El Garraoui Abdellah <elgarraoui.abdellah@oncf.ma>
2025-05-16 20:46:29 +0100

    Création d'entité JPA Product

---
+++ src/main/java/ma/iibdcc/appjee/entities/Product.java
@@ -0,0 +1,28 @@
+package ma.iibdcc.appjee.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class Product {
+    @Id @GeneratedValue
+    private Long id;
+    @NotEmpty
+    @Size(min = 3, max = 50)
+    private String name;
+    @Min(1)
+    private Double quantity;
+    @Min(0)
+    private Double price;
+}
