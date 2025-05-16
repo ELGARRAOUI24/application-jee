f32a2480cc8a0853a4c98f28b700528f3091b8e5
El Garraoui Abdellah <elgarraoui.abdellah@oncf.ma>
2025-05-16 20:38:58 +0100

    Création du projet Spring Boot avec les dépendances

---
+++ src/main/java/ma/iibdcc/appjee/AppJeeApplication.java
@@ -0,0 +1,13 @@
+package ma.iibdcc.appjee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppJeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppJeeApplication.class, args);
+    }
+
+}
