3f4d76ffbcb422bd0133c32fa259438cd56cde36
Bougarra <bougarra.02@gmail.com>
2025-06-01 08:50:56 +0100

    Spring MVC et vues thymeleaf : Affichage la liste des produits

---
+++ src/main/java/ma/iibdcc/appjee/web/ProductController.java
@@ -0,0 +1,24 @@
+package ma.iibdcc.appjee.web;
+
+import ma.iibdcc.appjee.entities.Product;
+import ma.iibdcc.appjee.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class ProductController {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/index")
+    public String index(Model model) {
+        List<Product> products = productRepository.findAll();
+        model.addAttribute("products", products);
+        return "products";
+    }
+}
